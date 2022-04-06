aeef2d5bf4d79fa8885069499a32f07fa449ead5
刘书豪 <liushuhao@cadg.cn>
2022-04-06 17:58:43 +0800

    feat：提交

---
+++ bindings.d.ts
@@ -0,0 +1,15 @@
+/*
+ * @Description: 
+ * @Version: 2.0
+ * @Autor: liushuhao
+ * @Date: 2022-04-04 23:17:50
+ * @LastEditors: liushuhao
+ * @LastEditTime: 2022-04-06 15:08:13
+ */
+export {};
+
+declare global {
+  const MY_ENV_VAR: string;
+  const MY_SECRET: string;
+  const MY_KV: KVNamespace;
+}
--- src/index.ts
+++ src/index.ts
@@ -4,39 +4,60 @@
  * @Autor: liushuhao
  * @Date: 2022-04-03 22:15:08
  * @LastEditors: liushuhao
- * @LastEditTime: 2022-04-04 04:19:52
+ * @LastEditTime: 2022-04-06 17:37:34
  */
+
+// const namespaces: any = ''
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS',
   'Access-Control-Max-Age': '86400',
 };
-addEventListener('fetch', event => {
-  const request = event.request;
-  const data = {
-    data: 'world',
-  };
 
-  const json = JSON.stringify(data, null, 2);
+async function handleRequest(request: Request) {
+  const value = await MY_KV.get("data", {type: 'json'})
+  console.log(value, 'value')
+  const params = {
+    code: 200,
+    success: true,
+    data: JSON.parse((JSON.stringify(value)))
+  }
+  console.log(params, 'params')
+  if (value === null) {
+    return new Response("Value not found", {
+      status: 404, headers: {
+        'Allow': 'GET, HEAD, POST, OPTIONS',
+        'content-type': 'application/json;charset=UTF-8',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS',
+      },
+    })
+  }
+
+  return new Response(JSON.stringify(params), {
+    headers: {
+      'Allow': 'GET, HEAD, POST, OPTIONS',
+      'content-type': 'application/json;charset=UTF-8',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS',
+    },
+  })
+}
+
+addEventListener('fetch', async event => {
+  const request = event.request;
   if (request.method === 'OPTIONS') {
     // Handle CORS preflight requests
     event.respondWith(handleOptions(request));
   } else if (request.method === 'GET' || request.method === 'HEAD' || request.method === 'POST') {
     // Handle requests to the API server
     return event.respondWith(
-      new Response(json, {
-        headers: {
-          'content-type': 'application/json;charset=UTF-8',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS',
-        },
-      })
+      handleRequest(request)
     );
   }
-
 });
 
-const handleOptions = (request: Request) =>{
+const handleOptions = (request: Request) => {
   const headers = request.headers;
   if (
     headers.get('Origin') !== null &&
